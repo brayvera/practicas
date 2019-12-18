856d507b5f8afdb7fe3c498649d15df0af556930
brayvera <56237070+brayvera@users.noreply.github.com>
2019-12-18 11:06:19 -0500

    Update conn.php

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'apsystem2');
+	$conn = new mysqli('localhost', 'bray', 'dragontigre2015', 'apsystem2');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
